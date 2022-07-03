860713b2591547c47167a0d230b77bde005d5c9b
zerocold242 <alexrylexx@gmail.com>
2022-07-03 13:46:46 +0300

    addColorSet, GlobalExtensions

---
+++ MyHabits/GlobalExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  GlobalExtensions.swift
+//  MyHabits
+//
+//  Created by Aleksey Lexx on 03.07.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let navigationColor = UIColor(named: "NavigationColor")
+    static let blueColor = UIColor(named: "BlueColor")
+    static let greenColor = UIColor(named: "GreenColor")
+    static let indigoColor = UIColor(named: "IndigoColor")
+    static let lightGrayColor = UIColor(named: "LightGrayColor")
+    static let orangeColor = UIColor(named: "OrangeColor")
+    static let purpleColor = UIColor(named: "PurpleColor")
+    
+}
