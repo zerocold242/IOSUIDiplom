1548aebec0d8cbaa9a0b761d982bd2773ddf135f
zerocold242 <alexrylexx@gmail.com>
2022-07-03 18:46:17 +0300

    добавлен текст в Info, констрейнты

--- MyHabits/Controllers/InfoViewController.swift
+++ MyHabits/Controllers/InfoViewController.swift
@@ -33,6 +33,31 @@ class InfoViewController: UIViewController {
         return label
     }()
     
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        label.textColor = .black
+        label.text = """
+
+Прохождение этапов, за которые за 21 день вырабатывается привычка, подчиняется следующему алгоритму:
+
+1. Провести 1 день без обращения к старым привычкам, стараться вести себя так, как будто цель, загаданная в перспективу, находится на расстоянии шага.
+
+2. Выдержать 2 дня в прежнем состоянии самоконтроля.
+
+3. Отметить в дневнике первую неделю изменений и подвести первые итоги — что оказалось тяжело, что — легче, с чем еще предстоит серьезно бороться.
+
+4. Поздравить себя с прохождением первого серьезного порога в 21 день. За это время отказ от дурных наклонностей уже примет форму осознанного преодоления и человек сможет больше работать в сторону принятия положительных качеств.
+
+5. Держать планку 40 дней. Практикующий методику уже чувствует себя освободившимся от прошлого негатива и двигается в нужном направлении с хорошей динамикой.
+
+6. На 90-й день соблюдения техники все лишнее из «прошлой жизни» перестает напоминать о себе, и человек, оглянувшись назад, осознает себя полностью обновившимся.
+"""
+        return label
+    }()
+    
     private func setupNavigationBar() {
         
         navigationItem.title = "Информация"
@@ -57,16 +82,23 @@ class InfoViewController: UIViewController {
             
             headerTextLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 22),
             headerTextLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-            headerTextLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16)
+            headerTextLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            
+            textLabel.topAnchor.constraint(equalTo: headerTextLabel.bottomAnchor, constant: 16),
+            textLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+            textLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+            textLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16)
         ])
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(headerTextLabel)
+        contentView.addSubview(textLabel)
         setupNavigationBar()
         setupConstraints()
     }
