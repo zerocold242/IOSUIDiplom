d5c74d97450cfbe108544ee2caddcf7495d505b3
zerocold242 <alexrylexx@gmail.com>
2022-07-04 20:00:20 +0300

    ScrollView, ContentView, constraints

--- MyHabits/Controllers/HabitViewController.swift
+++ MyHabits/Controllers/HabitViewController.swift
@@ -11,6 +11,18 @@ class HabitViewController: UIViewController {
     
     private var habit: Habit?
     
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     init (habit: Habit?) {
         self.habit = habit
         super.init(nibName: nil, bundle: nil)
@@ -19,11 +31,28 @@ class HabitViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupConstraints() {
+        
+        NSLayoutConstraint.activate ([
+            
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-
+        
     }
 }
