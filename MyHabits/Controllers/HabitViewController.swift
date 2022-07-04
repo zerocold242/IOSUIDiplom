e4ea48c16f12abbf936558fa30d0b1efea0d1863
zerocold242 <alexrylexx@gmail.com>
2022-07-04 23:01:28 +0300

    NavigationBar

--- MyHabits/Controllers/HabitViewController.swift
+++ MyHabits/Controllers/HabitViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class HabitViewController: UIViewController {
     
+    private lazy var appearance = UINavigationBarAppearance()
+    
     private var habit: Habit?
     
     private lazy var scrollView: UIScrollView = {
@@ -32,10 +34,47 @@ class HabitViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupNavigationBar() {
+        
+        if habit == nil {
+            navigationItem.title = "Создать"
+            navigationItem.largeTitleDisplayMode = .never
+            navigationController?.navigationBar.tintColor = .purpleColor
+            appearance.backgroundColor = .navigationColor
+            navigationController?.navigationBar.scrollEdgeAppearance = appearance
+            let rightBarButtonItem = UIBarButtonItem(title: "Сохранить",
+                                                     style: .done,
+                                                     target: self,
+                                                     action: #selector(saveButton))
+            navigationItem.rightBarButtonItem = rightBarButtonItem
+            let leftBarButtonItem = UIBarButtonItem(title: "Отменить",
+                                                    style: .done,
+                                                    target: self,
+                                                    action: #selector(cancelButton))
+            navigationItem.leftBarButtonItem = leftBarButtonItem
+            
+        } else {
+            
+            navigationItem.title = "Править"
+            navigationController?.navigationBar.tintColor = .purpleColor
+            appearance.backgroundColor = .navigationColor
+            navigationController?.navigationBar.scrollEdgeAppearance = appearance
+            let rightBarButtonItem = UIBarButtonItem(title: "Сохранить",
+                                                     style: .done,
+                                                     target: self,
+                                                     action: #selector(saveButton))
+            navigationItem.rightBarButtonItem = rightBarButtonItem
+            let leftBarButtonItem = UIBarButtonItem(title: "Отменить",
+                                                    style: .done,
+                                                    target: self,
+                                                    action: #selector(cancelButton))
+            navigationItem.leftBarButtonItem = leftBarButtonItem
+        }
+    }
+    
     private func setupConstraints() {
         
         NSLayoutConstraint.activate ([
-            
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -49,10 +88,18 @@ class HabitViewController: UIViewController {
         ])
     }
     
+    @objc private func saveButton() {}
+    
+    @objc private func cancelButton() {}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         
+        view.backgroundColor = .white
+        setupNavigationBar()
+        setupConstraints()
     }
 }
