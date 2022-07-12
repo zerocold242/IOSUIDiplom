f7c41684d8078af4d8325097a6be2e4e27bd2598
zerocold242 <alexrylexx@gmail.com>
2022-07-12 22:42:13 +0300

    доработка методов создания таббара

--- MyHabits/Controllers/InfoViewController.swift
+++ MyHabits/Controllers/InfoViewController.swift
@@ -9,14 +9,10 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    private lazy var appearance = UINavigationBarAppearance()
-    
     private func setupNavigationBar() {
         
         navigationItem.title = "Информация"
         navigationController?.navigationBar.isTranslucent = true
-        appearance.backgroundColor = .navigationColor
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
     override func viewDidLoad() {
