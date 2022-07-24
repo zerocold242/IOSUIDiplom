d89ebe73f7befd9d2ee3417daf02b2dc1fd537e4
zerocold242 <alexrylexx@gmail.com>
2022-07-24 21:45:57 +0300

    HabitCollectionViewCell/ timeLabel

--- MyHabits/Cells/HabitCollectionViewCell.swift
+++ MyHabits/Cells/HabitCollectionViewCell.swift
@@ -9,9 +9,56 @@ import UIKit
 
 class HabitCollectionViewCell: UICollectionViewCell {
     
+    private lazy var habitLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .headLine
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private lazy var timeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .caption
+        label.textColor = .systemGray2
+        return label
+    }()
+    
+    
+    
     var habitTrack: (() -> Void)?
     
     var habit: Habit?
+    
+    private func setupCell() {
+        contentView.addSubview(habitLabel)
+        contentView.addSubview(timeLabel)
+        
+        NSLayoutConstraint.activate([
+            habitLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            habitLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            habitLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -88),
+            
+            timeLabel.topAnchor.constraint(equalTo: habitLabel.bottomAnchor, constant: 4),
+            timeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            
+        ])
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.layer.cornerRadius = 10
+        contentView.backgroundColor = .white
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 extension HabitCollectionViewCell: CustomIdentifier {
