0eacdc375f989fbfcf6ce790a3068d6bf3f4a7c5
zerocold242 <alexrylexx@gmail.com>
2022-07-24 21:32:10 +0300

    ProgressCollectionViewCell/ ProgressView

--- MyHabits/Cells/ProgressCollectionViewCell.swift
+++ MyHabits/Cells/ProgressCollectionViewCell.swift
@@ -15,53 +15,66 @@ protocol CustomIdentifier: AnyObject {
 class ProgressCollectionViewCell: UICollectionViewCell {
     
     private lazy var titleLabel: UILabel = {
-          let label = UILabel()
-          label.translatesAutoresizingMaskIntoConstraints = false
-          label.text = "Всё получится!"
-          label.font = .footnoteStatus
-          label.textColor = .systemGray
-          return label
-      }()
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Всё получится!"
+        label.font = .footnoteStatus
+        label.textColor = .systemGray
+        return label
+    }()
     
     private lazy var percentLabel: UILabel = {
-          let label = UILabel()
-          label.translatesAutoresizingMaskIntoConstraints = false
-          label.font = .footnoteStatus
-          label.textColor = .systemGray
-          return label
-      }()
-
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .footnoteStatus
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.trackTintColor = .systemGray
+        progressView.progressTintColor = .purpleColor
+        return progressView
+    }()
+    
+    
     
     override init(frame: CGRect) {
-           super.init(frame: frame)
-           contentView.layer.cornerRadius = 10
-           contentView.backgroundColor = .white
-           setupViews()
-           refreshProgress()
-       }
-       
-       required init?(coder: NSCoder) {
-           fatalError("init(coder:) has not been implemented")
-       }
-
-
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 10
+        contentView.backgroundColor = .white
+        setupViews()
+        refreshProgress()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     private func setupViews() {
-          contentView.addSubview(titleLabel)
-          
-          NSLayoutConstraint.activate([
-              titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-              titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
-              
-              percentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-              percentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
-
-          ])
-      }
-
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            
+            percentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            percentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            
+            progressView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            progressView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            progressView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            progressView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+        ])
+    }
+    
     
     func refreshProgress() -> Void {
-        }
-
+    }
+    
     
 }
 
